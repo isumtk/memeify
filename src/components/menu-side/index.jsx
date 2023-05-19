55b6c6c11ba34dc300171b31171557db0020148c
Sumit Kumar <isumtk@gmail.com>
2023-05-19 13:34:24 +0530

    Added Meme Templates

--- src/components/menu-side/index.jsx
+++ src/components/menu-side/index.jsx
@@ -22,12 +22,12 @@ const MenuSide = () => {
 
 	return (
 		<aside className="relative flex h-full items-center rounded-s">
-			<div className="flex h-full w-14 flex-col items-center gap-2 bg-white pt-1">
+			<div className="flex h-full w-14 flex-col items-center gap-2 bg-gray-800 pt-1">
 				{menuButtons.map((button) => (
 					<button
 						onClick={() => activateMenu(button.name)}
 						className={`flex aspect-square w-12 items-center justify-center rounded border-transparent transition ${
-							menuState === button.name && 'bg-indigo-600'
+							menuState === button.name && 'bg-emerald-600'
 						}`}
 						key={button.name}
 					>
@@ -36,13 +36,13 @@ const MenuSide = () => {
 							style={{
 								fontSize: '24px',
 								transition: 'transform 0.1s ease',
-								color: menuState === button.name && 'rgb(255,255,255)',
+								color: 'rgb(255,255,255)',
 							}}
 						></ion-icon>
 					</button>
 				))}
 			</div>
-			<div className="h-full w-80 bg-slate-200 transition">
+			<div className="h-full w-80 bg-inherit transition">
 				{menuState === menuButtons[0].name && <DefaultMenu />}
 				{menuState === menuButtons[2].name && <TextMenu />}
 			</div>
--- src/components/menu-side/menus/default-menu/index.jsx
+++ src/components/menu-side/menus/default-menu/index.jsx
@@ -1,4 +1,6 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import axios from 'axios';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
+
 const allowedExtentions = ['jpeg', 'jpg', 'png'];
 
 const DefaultMenu = () => {
@@ -6,8 +8,9 @@ const DefaultMenu = () => {
 	const [fileName, setFileName] = useState('');
 	const [file, setFile] = useState({});
 	const [error, setError] = useState('');
+	const [memes, setMemes] = useState();
 
-	const validatFile = () => {
+	const validatFile = useCallback(() => {
 		setError('');
 		try {
 			const { name } = file;
@@ -29,31 +32,48 @@ const DefaultMenu = () => {
 				setError('Format is not supported');
 			}
 		}
-	};
+	}, [file, setError, setFileName]);
 
-	const handleSelectFile = (e) => {
-		const files = e.target.files;
-		if (files?.length > 0) {
-			setFile(files[0]);
+	const handleSelectFile = useCallback(
+		(e) => {
+			const files = e.target.files;
+			if (files?.length > 0) {
+				setFile(files[0]);
+			}
+		},
+		[setFile]
+	);
+
+	const fetchMemes = useCallback(async () => {
+		try {
+			const response = await axios.get('https://api.imgflip.com/get_memes');
+			setMemes(response.data);
+		} catch (error) {
+			console.log('Error fetching the memes');
 		}
-	};
+	}, []);
+
+	const memoizedMemes = useMemo(() => memes, [memes]);
+	useEffect(() => {
+		fetchMemes();
+	}, [fetchMemes]);
 
 	useEffect(() => {
-		console.log({ error });
-	}, [error]);
+		console.log({ memoizedMemes });
+	}, [memoizedMemes]);
 
 	useEffect(() => {
 		console.log(file?.name);
 	}, [file]);
 
 	return (
-		<section className="flex h-full w-full flex-col gap-2 overflow-y-auto overflow-x-clip bg-stone-200 p-3">
+		<section className="flex h-full w-full flex-col gap-2 overflow-y-auto overflow-x-clip bg-gray-900 p-3">
 			<section className="flex w-full flex-col items-center">
-				<div className="flex w-full items-center gap-2 font-normal text-gray-700">
+				<div className="flex w-full items-center gap-2 font-normal text-emerald-400">
 					<button
 						onClick={() => setIsUpload(false)}
 						className={`flex h-11 flex-1 items-center justify-center transition-all ${
-							!isUpload && 'font-medium text-blue-500'
+							!isUpload && 'font-medium text-emerald-500'
 						}`}
 					>
 						Browse
@@ -61,7 +81,7 @@ const DefaultMenu = () => {
 					<button
 						onClick={() => setIsUpload(true)}
 						className={`flex h-11 flex-1 items-center justify-center transition-all ${
-							isUpload && 'font-medium text-blue-500'
+							isUpload && 'font-medium text-emerald-500'
 						}`}
 					>
 						Select
@@ -69,20 +89,52 @@ const DefaultMenu = () => {
 				</div>
 				<div className="relative h-1 w-full">
 					<span
-						className={`absolute h-full w-1/2 rounded-md bg-blue-600 transition-transform ${
+						className={`absolute h-full w-1/2 rounded-md bg-emerald-600 transition-transform ${
 							isUpload && 'translate-x-full'
 						}`}
 					/>
 				</div>
 			</section>
-			<div className="relative flex w-full flex-1 overflow-x-clip rounded bg-white">
+			<div className="relative flex w-full flex-1 overflow-x-clip rounded bg-inherit">
 				<section
-					className={`absolute flex h-full w-full overflow-y-auto bg-rose-300 transition ${
+					className={`absolute flex h-full w-full overflow-y-auto bg-inherit transition ${
 						isUpload ? '-translate-x-full' : 'translate-x-0'
 					}`}
-				></section>
+				>
+					<div className="flex flex-1 flex-col gap-3 overflow-clip bg-inherit px-1 py-2">
+						{/* <div className="flex w-full items-center gap-2 rounded bg-white">
+							<input
+								type="text"
+								placeholder="Surprised Pikachu"
+								className="flex-1 rounded-s p-2 outline-none"
+							/>
+							<button className="flex aspect-square h-full items-center justify-center">
+								<ion-icon
+									name="search-sharp"
+									style={{ fontSize: '24px' }}
+								></ion-icon>
+							</button>
+						</div> */}
+						<div className="flex flex-1 flex-col gap-2 overflow-y-auto bg-inherit">
+							{memoizedMemes?.data?.memes.length > 0
+								? memoizedMemes.data.memes.map((meme) => (
+										<button
+											key={meme.id}
+											className="group relative aspect-square w-full overflow-clip outline-none"
+										>
+											<img
+												src={meme.url}
+												className="aspect-square w-full object-cover"
+											/>
+											<div className="absolute -z-10 h-full w-full bg-gray-500 group-hover:z-40"></div>
+										</button>
+								  ))
+								: ''}
+						</div>
+					</div>
+				</section>
 				<section
-					className={`absolute flex h-full w-full flex-col items-center justify-center bg-fuchsia-400 transition ${
+					className={`absolute flex h-full w-full flex-col items-center justify-center bg-inherit transition ${
 						isUpload ? 'translate-x-0' : 'translate-x-full'
 					}`}
 				>
@@ -97,11 +149,16 @@ const DefaultMenu = () => {
 							></input>
 							<label htmlFor="select_file" className="flex items-center gap-2">
 								<ion-icon
-									name="document-outline"
-									style={{ fontSize: '24px' }}
+									name="document-sharp"
+									style={{ fontSize: '32px', color: 'rgb(16 185 129)' }}
 								></ion-icon>
 							</label>
-							<button onClick={validatFile}>Select</button>
+							<button
+								onClick={validatFile}
+								className="text-lg font-medium text-emerald-500"
+							>
+								Select
+							</button>
 						</div>
 						<p
 							className={`w-4/5 text-center text-sm opacity-0 transition-opacity ${
